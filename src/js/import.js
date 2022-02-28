2af9fa3062542fdb3073c02e0efe7342a5d935b7
reynull <13519045@std.stei.itb.ac.id>
2022-02-28 17:21:21 +0700

    add export & import

--- src/js/import.js
+++ src/js/import.js
@@ -17,26 +17,28 @@ async function openfile() {
   let fileData = await fileHandle.getFile();
   let contents = await fileData.text();
 
-  json_file = JSON.parse(contents);
-
   // Deep copy vertex and colors
-  // Square
-  Squares.Vertices = [...json_file.square.Vertices];
-  Squares.Colors = [...json_file.square.Colors];
+  try {
+        json_file = JSON.parse(contents);
+        
+        // Square
+        Squares.Vertices = [...json_file.square.Vertices];
+        Squares.Colors = [...json_file.square.Colors];
 
-  // Rectangles
-  Rectangles.Vertices = [...json_file.rectangle.Vertices];
-  Rectangles.Colors = [...json_file.rectangle.Colors];
+        // Rectangles
+        Rectangles.Vertices = [...json_file.rectangle.Vertices];
+        Rectangles.Colors = [...json_file.rectangle.Colors];
 
-  // Lines
-  Lines.Vertices = [...json_file.lines.Vertices];
-  Lines.Colors = [...json_file.lines.Colors];
+        // Lines
+        Lines.Vertices = [...json_file.lines.Vertices];
+        Lines.Colors = [...json_file.lines.Colors];
 
-  // Polygons
-  Polygons.Vertices = [...json_file.polygon.Vertices];
-  Polygons.Colors = [...json_file.polygon.Colors];
-
-  // vertices = [...json_file.lines.Vertices]
+        // Polygons
+        Polygons.Vertices = [...json_file.polygon.Vertices];
+        Polygons.Colors = [...json_file.polygon.Colors];   
+    } catch (error) {
+        alert("File is corrupted / Not 2D Web-CAD save file")
+    }
 
   // Clear Canvas
   gl.clearColor(1, 1, 1, 1);
